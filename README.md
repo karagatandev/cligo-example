ff2b6391b8c0039586f3b3e5bc5e2585b6b23f99
karagatandev <alex@karagatan.com>
2025-03-22 13:23:19 -0700

    upload example

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # cligo-example
-CLIgo example
+
+The basic cligo example that has only one command.
+
+In majority applications especially in docker it could be totally enough.
+
+How to build
+```
+go build
+```
+
+How to run
+```
+./cligo-example run your_name --print
+```
+
+How to run with verbose
+```
+./cligo-example run your_name --print --verbose
+```
+
+How to run with help
+```
+./cligo-example --help
+./cligo-example run --help
+```
+
+How to run with version
+```
+./cligo-example --version
+```
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"go.arpabet.com/cligo"
+	"go.arpabet.com/glue"
+)
+
+type RunCommand struct {
+	Parent cligo.CliGroup `cli:"group=cli"`
+	Name   string         `cli:"argument=name"`
+	Print  bool           `cli:"option=print,default=false,help=prints it"`
+}
+
+func (cmd *RunCommand) Command() string {
+	return "run"
+}
+
+func (cmd *RunCommand) Help() (string, string) {
+	return "Runs the program.", `This command runs the program by using cligo framework.
+This framework is a game changer in the CLI tools.`
+}
+
+func (cmd *RunCommand) Run(ctx glue.Context) error {
+	if cmd.Print {
+		cligo.Echo("Invokes '%s' command with name '%s' argument", cmd.Command(), cmd.Name)
+	}
+	return nil
+}
+
+func main() {
+	cligo.Main(cligo.Beans(&RunCommand{}))
+}
